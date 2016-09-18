946e64e0df33ea14473990f106a7b14d0b5d2dfe
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2016-09-18 15:12:41 +0200

    Removed debug event...

--- src/main/java/me/ialistannen/navigator/Navigator.java
+++ src/main/java/me/ialistannen/navigator/Navigator.java
@@ -3,29 +3,17 @@ package me.ialistannen.navigator;
 import me.ialistannen.bukkitutil.commandsystem.base.CommandTree;
 import me.ialistannen.bukkitutil.commandsystem.implementation.DefaultCommandExecutor;
 import me.ialistannen.bukkitutil.commandsystem.implementation.DefaultTabCompleter;
-import me.ialistannen.bukkitutil.item.ItemStackBuilder;
-import me.ialistannen.bukkitutil.math.SphericalCoords;
 import me.ialistannen.languageSystem.I18N;
 import me.ialistannen.navigator.commands.CommandNavigator;
 import me.ialistannen.navigator.data.NavigationManager;
 import me.ialistannen.navigator.data.PlayerPoint;
 import me.ialistannen.navigator.data.PlayerPointManager;
-import me.ialistannen.navigator.gui.GuiItem;
-import me.ialistannen.navigator.gui.ItemClickResult;
 import me.ialistannen.navigator.gui.OpenedGuiManager;
-import me.ialistannen.navigator.gui.PageableGui;
-import me.ialistannen.navigator.navigatorlogic.RunningNavigator;
 import me.ialistannen.navigator.particles.ParticleManager;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -33,7 +21,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Locale;
 
-public final class Navigator extends JavaPlugin implements Listener {
+public final class Navigator extends JavaPlugin {
 
 	private static Navigator instance;
 
@@ -74,7 +62,6 @@ public final class Navigator extends JavaPlugin implements Listener {
 		playerPointManager = PlayerPointManager.loadOrGetNew(getDataFolder().toPath().resolve("users"));
 		navigationManager = new NavigationManager();
 
-		Bukkit.getPluginManager().registerEvents(this, this);
 		Bukkit.getPluginManager().registerEvents(guiManager, this);
 	}
 
@@ -140,43 +127,5 @@ public final class Navigator extends JavaPlugin implements Listener {
 	 */
 	public static Navigator getInstance() {
 		return instance;
-	}
-
-
-	@EventHandler
-	public void onLeftClick(PlayerInteractEvent e) {
-		if (e.getAction() != Action.LEFT_CLICK_AIR) {
-			return;
-		}
-
-		if (e.getPlayer().isSneaking()) {
-			PageableGui gui = new PageableGui(Bukkit.createInventory(null, 18), false, true);
-			for (int i = 0; i < 27; i++) {
-				final int j = i;
-				gui.addItem(new GuiItem(
-						ItemStackBuilder.builder(j < 9 ? Material.CHEST : Material.CAKE)
-								.setName("&6&l" + i)
-								.build(),
-						(clickEvent, gui1, player) -> {
-							e.getPlayer().sendMessage(j + "");
-							return new ItemClickResult(gui, ItemClickResult.ItemClickResultType.CANCEL);
-						}));
-			}
-			guiManager.openGui(e.getPlayer(), gui);
-			return;
-		}
-
-		Location endLoc = e.getPlayer().getEyeLocation();
-
-		SphericalCoords coords = new SphericalCoords(5,
-				Math.toRadians(endLoc.getYaw() + 90),
-				Math.toRadians(endLoc.getPitch() + 90));
-
-		endLoc.add(coords.toBukkitVector());
-
-		endLoc.setY(e.getPlayer().getLocation().getY());
-
-		RunningNavigator navigator = new RunningNavigator(e.getPlayer().getUniqueId(), endLoc, getNavigationManager());
-		navigator.start();
 	}
 }
